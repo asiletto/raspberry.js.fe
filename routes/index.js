84dd732cb99fb40d8e81ff16eeaa9931d10a4951
asiletto <alessandro.siletto@gmail.com>
2013-09-04 09:46:16 +0000

    inspiritas-bootstrap layout

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'raspberry.pi.js' });
 };
+
+exports.typography = function(req, res){
+  res.render('typography', { title: 'raspberry.pi.js/typography' });
+};
