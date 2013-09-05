403e66e4bff3fa056413e8e99cbb21524ca79795
Alessandro Siletto <alessandro@siletto.it>
2013-09-05 15:53:37 +0200

    test logo

--- logdao.js
+++ logdao.js
@@ -10,6 +10,12 @@ var ObjectID = require('mongodb').ObjectID;
 
 var LogDAO = function(){
   var self = this;
+  this.tables = {};
+  this.tables.config = "v2_config";
+  this.tables.raw = "";
+  this.tables.daily = "";
+  this.tables.hourly = "";
+  
   this.db = new mongodb.Db(process.env.DB_NAME, new mongodb.Server(process.env.DB_HOST_NAME, parseInt(process.env.DB_PORT), {auto_reconnect: true, poolSize: 2}), {native_parser: false, w:1});
   
   console.log("opening "+ process.env.DB_USER_NAME+":"+ process.env.DB_PASSWORD+"@"+ process.env.DB_HOST_NAME + ":" + parseInt(process.env.DB_PORT) + "/"+process.env.DB_NAME)
@@ -94,5 +100,8 @@ LogDAO.prototype.raw = function(series, callback) {
 
 };
 
+LogDAO.prototype.getSensors = function(callback){
+	this.adb.collection(this.tables.config).find({}, {sort:"order"}).toArray(callback);
+};
 
 exports.LogDAO = new LogDAO();
